c823de560d4b23295456d9d14469fb884e7b00e0
Felipe Mano <manofgpa@gmail.com>
2021-10-08 19:55:46 -0300

    Fix comments setAttribute async

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -6,7 +6,7 @@ export default class Comments extends Component {
     const anchor = document.getElementById('inject-comments-for-uterances')
     script.setAttribute('src', 'https://utteranc.es/client.js')
     script.setAttribute('crossorigin', 'anonymous')
-    script.setAttribute('async', true)
+    script.setAttribute('async', 'true')
     script.setAttribute('repo', 'Manofgpa/space-blog')
     script.setAttribute('issue-term', 'pathname')
     script.setAttribute('theme', 'dark-blue')
